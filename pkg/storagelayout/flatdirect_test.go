266dd1dc918e76c2826be94cb7c26cb92914d401
Juergen Enge <github@betalabs.ch>
2022-06-22 18:06:51 +0200

    initial commit

---
+++ pkg/storagelayout/flatdirect_test.go
@@ -0,0 +1,64 @@
+package storagelayout
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFlatDirectory(t *testing.T) {
+	// https://ocfl.github.io/extensions/0002-flat-direct-storage-layout.html
+	// Example 1
+	l := FlatDirect{}
+	objectID := "object-01"
+	testResult := "object-01"
+	rootPath, err := l.ID2Path(objectID)
+	if err != nil {
+		t.Errorf("cannot convert %s", objectID)
+	}
+	if rootPath != testResult {
+		t.Errorf("%s -> %s != %s", objectID, rootPath, testResult)
+	}
+	fmt.Printf("FlatDirect(%s) -> %s\n", objectID, rootPath)
+
+	objectID = "..hor_rib:lé-$id"
+	testResult = "..hor_rib:lé-$id"
+	rootPath, err = l.ID2Path(objectID)
+	if err != nil {
+		t.Errorf("cannot convert %s - %v", objectID, err)
+	} else {
+		if rootPath != testResult {
+			t.Errorf("%s -> %s != %s", objectID, rootPath, testResult)
+		} else {
+			fmt.Printf("FlatDirect(%s) -> %s\n", objectID, rootPath)
+		}
+	}
+
+	// https://ocfl.github.io/extensions/0002-flat-direct-storage-layout.html
+	// Example 2
+	objectID = "info:fedora/object-01"
+	testResult = "info:fedora/object-01"
+	rootPath, err = l.ID2Path(objectID)
+	if err != nil {
+		t.Errorf("cannot convert %s - %v", objectID, err)
+	} else {
+		if rootPath != testResult {
+			t.Errorf("%s -> %s != %s", objectID, rootPath, testResult)
+		} else {
+			fmt.Printf("FlatDirect(%s) -> %s\n", objectID, rootPath)
+		}
+	}
+
+	objectID = "abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij"
+	testResult = "abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij"
+	rootPath, err = l.ID2Path(objectID)
+	if err != nil {
+		t.Errorf("cannot convert %s - %v", objectID, err)
+	} else {
+		if rootPath != testResult {
+			t.Errorf("%s -> %s != %s", objectID, rootPath, testResult)
+		} else {
+			fmt.Printf("FlatDirect(%s) -> %s\n", objectID, rootPath)
+		}
+	}
+
+}
