0829738f7cc6f967091df0405088f2fcdc441e5e
Juergen Enge <you@example.com>
2024-09-24 17:04:45 +0200

    registered extension 0011

--- data/defaultextensions/object/embed.go
+++ data/defaultextensions/object/embed.go
@@ -4,6 +4,6 @@ import "embed"
 
 //go:embed NNNN-filesystem/config.json
 //go:embed NNNN-indexer/config.json
-//go:embed NNNN-direct-clean-path-layout/config.json
+//go:embed 0011-direct-clean-path-layout/config.json
 //go:embed 0001-digest-algorithms/config.json
 var DefaultObjectExtensionFS embed.FS
