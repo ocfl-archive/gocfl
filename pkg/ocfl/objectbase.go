f6d53417944650f0475c8f2a80d6577c78c5c570
Juergen Enge <you@example.com>
2025-06-04 19:16:44 +0200

    checksum inventory with filename

--- pkg/ocfl/objectbase.go
+++ pkg/ocfl/objectbase.go
@@ -437,7 +437,7 @@ func (object *ObjectBase) StoreInventory(version bool, objectRoot bool) error {
 		if err != nil {
 			return errors.Wrapf(err, "cannot create '%v/%s'", object.fsys, csFileName)
 		}
-		if _, err := iCSWriter.Write([]byte(checksumString)); err != nil {
+		if _, err := iCSWriter.Write([]byte(checksumString + " inventory.json")); err != nil {
 			iCSWriter.Close()
 			return errors.Wrapf(err, "cannot write to '%s'", csFileName)
 		}
