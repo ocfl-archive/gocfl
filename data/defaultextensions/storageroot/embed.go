e7db77f100480bda24ac06c72adcf99906ead302
Juergen Enge <you@example.com>
2025-12-05 17:33:51 +0100

    Added new embed directives for storage root extensions

--- data/defaultextensions/storageroot/embed.go
+++ data/defaultextensions/storageroot/embed.go
@@ -4,5 +4,7 @@ import "embed"
 
 // go:embed initial/*.json
 //
-//go:embed NNNN-direct-clean-path-layout
+//go:embed NNNN-direct-clean-path-layout/*.json
+//go:embed NNNN-gocfl-extension-manager/*.json
+//go:embed initial/*.json
 var DefaultStorageRootExtensionFS embed.FS
