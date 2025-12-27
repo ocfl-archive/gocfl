d9ac1a98c8a1425029c1c7f15b83bd0c3ece0402
Juergen Enge <you@example.com>
2025-12-27 20:25:54 +0100

    Added comprehensive tests for `State` and `User` JSON (un)marshalling, validation, and state operations. Introduced `DummyValidation` implementation with error and warning handling for validation scenarios.

--- pkg/ocfl/inventory/user11_test.go
+++ pkg/ocfl/inventory/user11_test.go
@@ -2,21 +2,78 @@ package inventory
 
 import (
 	"encoding/json"
+	"reflect"
 	"testing"
+
+	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/validation"
 )
 
-var factory = NewFactory11()
+func Test_UserJSONMarshal(t *testing.T) {
+	user := factory11.NewUser().WithName("Alice").WithAddress("mailto:alice@example.org")
+	bytes, err := json.Marshal(user)
+	if err != nil {
+		t.Fatalf("Failed to marshal user: %s", err)
+	}
+	user2 := factory11.NewUser()
+	if err := json.Unmarshal(bytes, user2); err != nil {
+		t.Fatalf("Failed to unmarshal user: %s", err)
+	}
+	if !reflect.DeepEqual(user, user2) {
+		t.Fatalf("Failed to unmarshal user - not equal")
+	}
+}
 
-func Test_UserJSON(t *testing.T) {
+func Test_UserJSONUnmarshal(t *testing.T) {
 	var jsonData = []byte(`
 {
 	"address": "mailto:alice@example.org",
 	"name": "Alice"
 }
 `)
-	user := factory.NewUser()
+	user := factory11.NewUser()
 	if err := json.Unmarshal(jsonData, user); err != nil {
 		t.Errorf("unmarshal error: %v", err)
 	}
+	name, err := user.GetName()
+	if err != nil {
+		t.Errorf("get name error: %v", err)
+	}
+	if name != "Alice" {
+		t.Errorf("name error: %v", name)
+	}
+	address, err := user.GetAddress()
+	if err != nil {
+		t.Errorf("get address error: %v", err)
+	}
+	if address != "mailto:alice@example.org" {
+		t.Errorf("address error: %v", address)
+	}
+}
 
+func Test_UserInvalidJSON(t *testing.T) {
+	var jsonData = []byte(`
+{
+	"address": 42,
+	"name": "Alice"
+}
+`)
+	user := factory11.NewUser()
+	if err := json.Unmarshal(jsonData, user); err != nil {
+		t.Errorf("unmarshal error: %v", err)
+	}
+	if user.Err() == nil {
+		t.Error("should get an error")
+	}
+}
+
+func Test_UserInvalidAddressCheck(t *testing.T) {
+	val := NewDummyValidation()
+	user := factory11.NewUser().WithAddress("xxx")
+	if err := user.Check(val, "v1"); err != nil {
+		t.Fatalf("check error: %v", err)
+	}
+
+	if !val.HasWarning(validation.W009) {
+		t.Errorf("no warning '%s' in %v", validation.W009, val.Warning)
+	}
 }
