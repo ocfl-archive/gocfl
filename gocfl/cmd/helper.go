8921411fc4267a238a5718dfd4a1fa276b0fd397
Juergen Enge <you@example.com>
2026-02-16 11:22:17 +0100

    Moved `CreateStorageRoot` and related functions from `storagerootf.go` to `helper.go` for better organization and to consolidate related functionality. Removed `storagerootf.go`.

--- gocfl/cmd/helper.go
+++ gocfl/cmd/helper.go
@@ -22,11 +22,16 @@ import (
 	ocflextension "github.com/ocfl-archive/gocfl/v2/pkg/extension"
 	extensiontypes "github.com/ocfl-archive/gocfl/v2/pkg/ocfl/extension"
 	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/extension/extensionimpl"
+	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/factory"
+	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/factory/factoryimpl"
 	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/functions"
 	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/object"
+	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/ocflerrors"
 	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/storageroot"
 	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/storageroot/storagerootimpl"
+	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/util"
 	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/validation"
+	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/version"
 	"github.com/ocfl-archive/gocfl/v2/pkg/subsystem/migration"
 	"github.com/ocfl-archive/gocfl/v2/pkg/subsystem/thumbnail"
 	ironmaiden "github.com/ocfl-archive/indexer/v3/pkg/indexer"
@@ -410,4 +415,65 @@ func addObjectByPath(
 	}
 
 	return o.IsModified(), nil
+}
+
+func CreateStorageRoot(ctx context.Context, fsys fs.FS, ver version.OCFLVersion, extensionFactory *extensionimpl.ExtensionFactory, extensionManager storagerootimpl.ExtensionManager, digest checksum.DigestAlgorithm, logger zLogger.ZLogger) (storageroot.StorageRoot, error) {
+	fact := factoryimpl.NewFactory(ver, extensionFactory, logger)
+	storageRoot := fact.NewStorageRoot(ctx).WithFS(fsys)
+
+	if err := storageRoot.Init(ver, digest, extensionManager); err != nil {
+		return nil, errors.Wrap(err, "cannot initialize storage root")
+	}
+
+	return storageRoot, nil
+}
+
+func LoadStorageRoot(ctx context.Context, fsys fs.FS, extensionFactory *extensionimpl.ExtensionFactory, logger zLogger.ZLogger) (storageroot.StorageRoot, error) {
+	ver, err := util.GetVersion(ctx, fsys, ".", "ocfl_")
+	if err != nil && !errors.Is(err, ocflerrors.ErrVersionNone) {
+		return nil, errors.WithStack(err)
+	}
+	if ver == "" {
+		dirs, err := fs.ReadDir(fsys, ".")
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		if len(dirs) > 0 {
+			err := validation.GetValidationError(version.Version1_1, validation.E069).AppendDescription("storage root not empty without version information").AppendContext("storage root '%s'", fsys)
+			validation.AddValidationErrors(ctx, err)
+			//			return nil, err
+		}
+		ver = version.Version1_1
+	}
+	fact := factoryimpl.NewFactory(ver, extensionFactory, logger)
+	storageRoot := fact.NewStorageRoot(ctx).WithFS(fsys)
+	if err := storageRoot.Load(); err != nil {
+		return nil, errors.Wrap(err, "cannot load storage root")
+	}
+	return storageRoot, nil
+}
+
+func LoadStorageRootRO(ctx context.Context, fact factory.Factory, fsys fs.FS, extensionFactory *extensionimpl.ExtensionFactory, logger zLogger.ZLogger) (storageroot.StorageRoot, error) {
+	ver, err := util.GetVersion(ctx, fsys, ".", "ocfl_")
+	if err != nil && !errors.Is(err, ocflerrors.ErrVersionNone) {
+		return nil, errors.WithStack(err)
+	}
+	if ver == "" {
+		dirs, err := fs.ReadDir(fsys, ".")
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		if len(dirs) > 0 {
+			err := validation.GetValidationError(version.Version1_1, validation.E069).AppendDescription("storage root not empty without version information").AppendContext("storage root '%s'", fsys)
+			validation.
+				AddValidationErrors(ctx, err)
+			//			return nil, err
+		}
+		ver = version.Version1_1
+	}
+	storageRoot := fact.NewStorageRoot(ctx).WithFS(fsys)
+	if err := storageRoot.Load(); err != nil {
+		return nil, errors.Wrap(err, "cannot load storage root")
+	}
+	return storageRoot, nil
 }
