51f724ac18a613108c5324a172ee65f049e9771b
Juergen Enge <you@example.com>
2026-02-17 16:43:49 +0100

    Set `Version1_1` as the fallback OCFL version in `factory.go` and removed unused `Version1_1` switch case logic.

--- pkg/ocfl/factory/factoryimpl/factory.go
+++ pkg/ocfl/factory/factoryimpl/factory.go
@@ -1,8 +1,6 @@
 package factoryimpl
 
 import (
-	"fmt"
-
 	"github.com/je4/utils/v2/pkg/zLogger"
 	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/extension/extensionimpl"
 	factorytypes "github.com/ocfl-archive/gocfl/v2/pkg/ocfl/factory"
@@ -13,11 +11,11 @@ func NewFactory(ver version.OCFLVersion, extensionFactory *extensionimpl.Extensi
 	switch ver {
 	case version.Version1_0:
 		return NewFactory10(extensionFactory, logger)
-	case version.Version1_1:
-		return NewFactory11(extensionFactory, logger)
+		//	case version.Version1_1:
+		//		return NewFactory11(extensionFactory, logger)
 	case version.Version2_0:
 		return NewFactory20(extensionFactory, logger)
 	default:
-		panic(fmt.Sprintf("Unsupported version: %v", ver))
+		return NewFactory11(extensionFactory, logger)
 	}
 }
