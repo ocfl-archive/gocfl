72230831f1ba9cbe3a45c204a89481099afa5300
Juergen Enge <you@example.com>
2026-02-17 16:56:38 +0100

    Consolidated `storagerootimpl` into `storageroot` by renaming package and updating imports to simplify structure and improve maintainability.

--- pkg/ocfl/storageroot/storagerootExtension.go
+++ pkg/ocfl/storageroot/storagerootExtension.go
@@ -1,14 +1,13 @@
-package storagerootimpl
+package storageroot
 
 import (
 	"io/fs"
 
 	extensiontypes "github.com/ocfl-archive/gocfl/v2/pkg/ocfl/extension"
-	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/storageroot"
 )
 
 type ExtensionStorageRootPath interface {
 	extensiontypes.Extension
 	WriteLayout(fsys fs.FS) error
-	BuildStorageRootPath(storageRoot storageroot.StorageRoot, id string) (string, error)
+	BuildStorageRootPath(storageRoot StorageRoot, id string) (string, error)
 }
--- pkg/ocfl/storageroot/storagerootExtensionManager.go
+++ pkg/ocfl/storageroot/storagerootExtensionManager.go
@@ -1,4 +1,4 @@
-package storagerootimpl
+package storageroot
 
 import (
 	"github.com/ocfl-archive/gocfl/v2/pkg/ocfl/extension"
