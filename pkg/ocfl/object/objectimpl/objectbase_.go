bd07e4f9ffedea95ee86218e39f8d1e36766acb4
Juergen Enge <you@example.com>
2026-02-22 11:09:03 +0100

    Removed unused `objectbase_` implementation from `objectimpl` for cleanup and maintainability.

--- pkg/ocfl/object/objectimpl/objectbase_.go
+++
@@ -1,7 +0,0 @@
-package objectimpl
-
-//const VERSION = "1.0"
-
-//var objectConformanceDeclaration = fmt.Sprintf("0=ocfl_object_%s", VERSION)
-
-// todo: check WithWriteable() and repair incorrect use...
