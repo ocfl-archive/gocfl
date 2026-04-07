3aba45d5f7b72f18c31e72048fd12186c05b4a23
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2026-04-07 16:46:13 +0200

    Add doc.go to object package and basic godoc

---
+++ pkg/ocfl/object/doc.go
@@ -0,0 +1,15 @@
+/*
+Package object defines the interfaces required by GOCFL to create
+and interact with multiple versions of an OCFL "object" type.
+
+Interfaces for extension related object functions are also defined
+in this package and are prefixed with:
+
+	type Extension...
+
+Examples include:
+
+	type ExtensionObjectContentPath interface { ... }
+	type ExtensionObjectExtractPath interface { ... |
+*/
+package object
