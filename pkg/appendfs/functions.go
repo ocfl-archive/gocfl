29f64ae30f7d0af4f60c057c9aa3618c9d02a279
Juergen Enge <you@example.com>
2026-05-11 17:11:45 +0200

    Refactored `writefs.SubFSCreate` to `writefs.SubCreate` in `appendfs` for consistency with updated API.

--- pkg/appendfs/functions.go
+++ pkg/appendfs/functions.go
@@ -11,7 +11,7 @@ func Sub(fsys FS, path string) (FS, error) {
 	if fsys == nil {
 		return nil, errors.New("fsys is nil")
 	}
-	newFS, err := writefs.SubFSCreate(fsys, path)
+	newFS, err := writefs.SubCreate(fsys, path)
 	if err != nil {
 		return nil, errors.Wrapf(err, "subfs %v/%s", fsys, path)
 	}
